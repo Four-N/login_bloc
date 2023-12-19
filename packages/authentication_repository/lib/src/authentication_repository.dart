6a570252c98e23db1454b66cc1edada30cfb663e
Four-N <nathaphon.v@gmail.com>
2023-12-19 14:41:17 +0700

    authenBloc

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+
+class AuthenticationRepository {
+  final _controller = StreamController<AuthenticationStatus>();
+
+  Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unauthenticated;
+    yield* _controller.stream;
+  }
+
+  Future<void> logIn({
+    required String username,
+    required String password,
+  }) async {
+    await Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _controller.add(AuthenticationStatus.authenticated),
+    );
+  }
+
+  void logOut() {
+    _controller.add(AuthenticationStatus.unauthenticated);
+  }
+
+  void dispose() => _controller.close();
+}
+//AuthenticationRepository จะ exposes Stream ของ AuthenticationStatus อัพเดตเพื่อที่จะใช้สำรหรับแจ้งตัวApp เมื่อ user เข้าใช้หรือออก
+//
